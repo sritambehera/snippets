9987d2731bb5f7a37bfb8ed7d7ec70e810c7d458
Sritam Behera <sritambehera110@gmail.com>
2019-09-18 18:07:23 +0530

    auths & permissions

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,4 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
+]
+
+urlpatterns +=[
+    path('api-auth/', include('rest_framework.urls')),
 ]
